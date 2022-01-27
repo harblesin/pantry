d749c86dc1415a2b64fdad4f22ced7a229dcc508
ylech <waltermelonson@gmail.com>
2022-01-26 23:06:25 -0700

    Created react app and began setup

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+const Header = (props) => {
+    return (
+        <header className={styles.headerBody}>
+            Here is a header
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Pages/Home.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import logo from '../logo.svg';
+import styles from "./Home.module.css";
+import Header from "../Components/Header";
+
+class Home extends Component {
+    state = {
+
+    }
+
+
+    render = () => {
+        return (
+            <div className={`${styles.HomeBody}`} >
+                <Header />
+
+
+
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+
+
+            </div>
+        )
+    }
+}
+
+export default Home
